2eccf62f55190968599837628b25c05425cae0b2
Gaurav Thakur <gthakur581@gmail.com>
2022-07-02 10:50:25 +0530

    setup eslint/prettier rules

--- src/components/FancyButton/FancyButton.tsx
+++ src/components/FancyButton/FancyButton.tsx
@@ -1,19 +1,23 @@
-import React, {ForwardedRef, forwardRef, HTMLAttributes} from 'react';
+import React, { ForwardedRef, forwardRef, HTMLAttributes } from 'react';
 import './FancyButton.css';
 
 export interface ButtonProps extends HTMLAttributes<HTMLElement> {
     href?: string;
 }
 
-export const FancyButton = forwardRef<HTMLButtonElement | HTMLAnchorElement, ButtonProps>(
-    ({className, href, ...props}, ref) => {
-        const classes = className ? 'fancy-button ' + className.trim(): 'fancy-button';
-        if (href) return <a {...props} href={href} className={classes} />
-        return (<button
+export const FancyButton = forwardRef<
+    HTMLButtonElement | HTMLAnchorElement,
+    ButtonProps
+>(({ className, href, ...props }, ref) => {
+    const classes = className
+        ? 'fancy-button ' + className.trim()
+        : 'fancy-button';
+    if (href) return <a {...props} className={classes} href={href} />;
+    return (
+        <button
             className={classes}
             {...props}
             ref={ref as ForwardedRef<HTMLButtonElement>}
-        />)
-    },
-);
-
+        />
+    );
+});
