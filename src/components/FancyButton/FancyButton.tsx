7190a2b1d12656019245b7f090f9f706c79a8db9
Gaurav Thakur <gthakur581@gmail.com>
2022-07-02 10:33:25 +0530

    add normalize css

--- src/components/FancyButton/FancyButton.tsx
+++ src/components/FancyButton/FancyButton.tsx
@@ -1,16 +1,18 @@
-import React, {ButtonHTMLAttributes, forwardRef} from 'react';
+import React, {ForwardedRef, forwardRef, HTMLAttributes} from 'react';
 import './FancyButton.css';
 
-export interface ButtonProps extends ButtonHTMLAttributes<HTMLButtonElement> {
+export interface ButtonProps extends HTMLAttributes<HTMLElement> {
+    href?: string;
 }
 
-export const FancyButton = forwardRef<HTMLButtonElement, ButtonProps>(
-    ({className, ...props}, ref) => {
+export const FancyButton = forwardRef<HTMLButtonElement | HTMLAnchorElement, ButtonProps>(
+    ({className, href, ...props}, ref) => {
         const classes = className ? 'fancy-button ' + className.trim(): 'fancy-button';
+        if (href) return <a {...props} href={href} className={classes} />
         return (<button
             className={classes}
             {...props}
-            ref={ref}
+            ref={ref as ForwardedRef<HTMLButtonElement>}
         />)
     },
 );
--- src/stories/FancyButton.stories.tsx
+++ src/stories/FancyButton.stories.tsx
@@ -2,22 +2,15 @@ import React from 'react';
 import { ComponentStory, ComponentMeta } from '@storybook/react';
 import {FancyButton} from '../components';
 
-
-// More on default export: https://storybook.js.org/docs/react/writing-stories/introduction#default-export
 export default {
   title: 'Components/Button/Fancy Button',
   component: FancyButton,
-  // More on argTypes: https://storybook.js.org/docs/react/api/argtypes
-  argTypes: {
-    backgroundColor: { control: 'color' },
-  },
 } as ComponentMeta<typeof FancyButton>;
 
-// More on component templates: https://storybook.js.org/docs/react/writing-stories/introduction#using-args
 const Template: ComponentStory<typeof FancyButton> = (args) => <FancyButton {...args} />;
 
-export const Primary = Template.bind({});
-// More on args: https://storybook.js.org/docs/react/writing-stories/args
-Primary.args = {
-  children: 'Primary Button'
+export const Button = Template.bind({});
+Button.args = {
+  children: 'Fancy Button',
+  href: '#',
 };
