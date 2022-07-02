e32d739014ff322ddf65238141f0ca4bf48533e5
Gaurav Thakur <gthakur581@gmail.com>
2022-07-02 14:29:57 +0530

    fix the styling of button when element is anchor tag

--- src/stories/FancyButton.stories.tsx
+++ src/stories/FancyButton.stories.tsx
@@ -1,16 +1,19 @@
 import React from 'react';
 import { ComponentStory, ComponentMeta } from '@storybook/react';
-import {FancyButton} from '../components';
+import { FancyButton } from '../components';
 
 export default {
-  title: 'Components/Button/Fancy Button',
-  component: FancyButton,
+    title: 'Components/Button/Fancy Button',
+    component: FancyButton,
 } as ComponentMeta<typeof FancyButton>;
 
-const Template: ComponentStory<typeof FancyButton> = (args) => <FancyButton {...args} />;
+const Template: ComponentStory<typeof FancyButton> = (args) => (
+    <FancyButton {...args} />
+);
 
 export const Button = Template.bind({});
 Button.args = {
-  children: 'Fancy Button',
-  href: '#',
+    children: 'Fancy Button',
+    href: '#',
+    style: { width: 200 },
 };
